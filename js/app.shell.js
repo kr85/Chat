af14a372a8f680655d0c43829e4f0cb7123e1b77
Kosta Rashev <kosta.rashev@gmail.com>
2015-03-31 20:57:06 -0700

    Use jquery urianchor to support browser bookmarks, refreshes, forward and backward navigations

--- js/app.shell.js
+++ js/app.shell.js
@@ -7,6 +7,12 @@
 /*global $, app */
 app.shell = (function () {
   var configMap = {
+        anchor_schema_map : {
+          chat : {
+            open   : true,
+            closed : true
+          }
+        },
         main_html : String()
           + '<div class="app-shell-header">'
             + '<div class="app-shell-header-logo"></div>'
@@ -29,10 +35,20 @@ app.shell = (function () {
       },
       stateMap = {
         $container        : null,
+        anchor_map        : {},
         is_chat_retracted : true
       },
       jQueryMap = {},
-      setjQueryMap, toggleChat, onClickChat, initModule;
+      copyAnchorMap, setjQueryMap, toggleChat, changeAnchorPart, onHashChange,
+      onClickChat, initModule;
+
+  copyAnchorMap = function () {
+    return $.extend(
+      true,
+      {},
+      stateMap.anchor_map
+    );
+  };
 
   setjQueryMap = function () {
     var $container = stateMap.$container;
@@ -80,8 +96,77 @@ app.shell = (function () {
     return true;
   };
 
+  changeAnchorPart = function (arg_map) {
+    var anchor_map_revise = copyAnchorMap(),
+        bool_return = true,
+        key_name, key_name_dep;
+
+    for (key_name in arg_map) {
+      if (arg_map.hasOwnProperty(key_name)) {
+        if (key_name.indexOf('_') === 0) {
+          continue;
+        }
+
+        anchor_map_revise[key_name] = arg_map[key_name];
+        key_name_dep = '_' + key_name;
+
+        if (arg_map[key_name_dep]) {
+          anchor_map_revise[key_name_dep] = arg_map[key_name_dep];
+        } else {
+          delete anchor_map_revise[key_name_dep];
+          delete anchor_map_revise['_s' + key_name_dep];
+        }
+      }
+    }
+
+    try {
+      $.uriAnchor.setAnchor(anchor_map_revise);
+    } catch (error) {
+      $.uriAnchor.setAnchor(stateMap.anchor_map, null, true);
+      bool_return = false;
+    }
+
+    return bool_return;
+  };
+
+  onHashChange = function () {
+    var anchor_map_previous = copyAnchorMap(),
+        anchor_map_proposed,
+        _s_chat_previous, _s_chat_proposed, s_chat_proposed;
+
+    try {
+      anchor_map_proposed = $.uriAnchor.makeAnchorMap();
+    } catch (error) {
+      $.uriAnchor.setAnchor(anchor_map_previous, null, true);
+      return false;
+    }
+    stateMap.anchor_map = anchor_map_proposed;
+
+    _s_chat_previous = anchor_map_previous._s_chat;
+    _s_chat_proposed = anchor_map_proposed._s_chat;
+
+    if (!anchor_map_previous || _s_chat_previous !== _s_chat_proposed) {
+      s_chat_proposed = anchor_map_proposed.chat;
+      switch (s_chat_proposed) {
+        case 'open' :
+          toggleChat(true);
+          break;
+        case 'closed' :
+          toggleChat(false);
+          break;
+        default :
+          toggleChat(false);
+          delete anchor_map_proposed.chat;
+          $.uriAnchor.setAnchor(anchor_map_proposed, null, true);
+      }
+    }
+    return false;
+  };
+
   onClickChat = function () {
-    toggleChat(stateMap.is_chat_retracted);
+    changeAnchorPart({
+      chat : (stateMap.is_chat_retracted) ? 'open' : 'closed'
+    });
     return false;
   };
 
@@ -94,6 +179,12 @@ app.shell = (function () {
     jQueryMap.$chat
       .attr('title', configMap.chat_retracted_title)
       .click(onClickChat);
+
+    $.uriAnchor.configModule({
+      schema_map : configMap.anchor_schema_map
+    });
+
+    $(window).bind('hashchange', onHashChange).trigger('hashchange');
   };
 
   return {
